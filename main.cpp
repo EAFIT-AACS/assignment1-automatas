bab8985cde37aa5ca8873285103cd06ae4897b5a
Salazar1022 <ssalazarh3@eafit.edu.co>
2025-02-23 16:41:59 -0500

    Se agrega la funcion "CheckStateEquivalence" para poder determinar que estados son equivalentes y cuales no. Se hacen cambios en el main para poder mostrar dichos estados

--- main.cpp
+++ main.cpp
@@ -5,24 +5,28 @@
 #include <sstream>
 #include <vector>
 #include <algorithm>
+#include <map>
+#include <set>
 
 using namespace std;
 
 // Structure
-struct Automata {
+struct Automata
+{
     int number_states;
     vector<string> alphabet;
     vector<int> finals;
-    vector<vector <int>> functions;
+    vector<vector<int>> functions;
 };
 
 // Automata´s Dinamic Array
 vector<Automata> automatasArray;
 
 // Functions
-void loadDfas(int num_cases, ifstream& file, vector<Automata>& automatasArray);
-void showDfasArray(const vector<Automata>& automatas);
-void separateStates(Automata dfa);
+void loadDfas(int num_cases, ifstream &file, vector<Automata> &automatasArray);
+void showDfasArray(const vector<Automata> &automatas);
+vector<int> separateStates(Automata dfa);
+void checkStateEquivalence(const Automata& dfa);
 
 int main()
 {
@@ -43,20 +47,24 @@ int main()
 
     // Load the DFA's from the txt and load them into the Automatas' Array
     loadDfas(num_cases, file, automatasArray);
-    
+
     // Show the DFA's
-    showDfasArray(automatasArray);
-    
-    separateStates(automatasArray[0]);
+    // showDfasArray(automatasArray);
+
+    for (const auto& dfa : automatasArray) {
+        checkStateEquivalence(dfa);
+    }
 
     file.close();
-    
+
     return 0;
 }
 
-void loadDfas(int num_cases, ifstream& file, vector<Automata>& automatasArray){
+void loadDfas(int num_cases, ifstream &file, vector<Automata> &automatasArray)
+{
 
-    for (int i = 0; i < num_cases; i++) {
+    for (int i = 0; i < num_cases; i++)
+    {
         int num_states;
 
         file >> num_states;
@@ -68,19 +76,23 @@ void loadDfas(int num_cases, ifstream& file, vector<Automata>& automatasArray){
         getline(file, line);
         stringstream ss(line);
         vector<string> alphabetDFA;
-        while (ss >> symbol) alphabetDFA.push_back(symbol);
+        while (ss >> symbol)
+            alphabetDFA.push_back(symbol);
 
         // Read final states
         getline(file, line);
         stringstream ss2(line);
         vector<int> final_states;
         int state;
-        while (ss2 >> state) final_states.push_back(state);
+        while (ss2 >> state)
+            final_states.push_back(state);
 
         // Read Transition Table
         vector<vector<int>> transition_table(num_states, vector<int>(alphabetDFA.size()));
-        for (int j = 0; j < num_states; j++) {
-            for (int k = 0; k < alphabetDFA.size(); k++) {
+        for (int j = 0; j < num_states; j++)
+        {
+            for (int k = 0; k < alphabetDFA.size(); k++)
+            {
                 file >> transition_table[j][k];
             }
         }
@@ -96,24 +108,29 @@ void loadDfas(int num_cases, ifstream& file, vector<Automata>& automatasArray){
     }
 }
 
-void showDfasArray(const vector<Automata>& automatasArray){
+void showDfasArray(const vector<Automata> &automatasArray)
+{
 
     for (int i = 0; i < automatasArray.size(); i++)
     {
         cout << "\nDFA " << i + 1 << " - States: " << automatasArray[i].number_states << endl;
 
         cout << "Alphabet: ";
-        for (const string &s : automatasArray[i].alphabet) cout << s << " ";
+        for (const string &s : automatasArray[i].alphabet)
+            cout << s << " ";
         cout << endl;
 
         cout << "Final States: ";
-        for (int s : automatasArray[i].finals) cout << s << " ";
+        for (int s : automatasArray[i].finals)
+            cout << s << " ";
         cout << endl;
 
         cout << "Transitions:" << endl;
-        for (int j = 0; j < automatasArray[i].number_states; j++) {
+        for (int j = 0; j < automatasArray[i].number_states; j++)
+        {
             cout << "Estado " << j << ": ";
-            for (int k = 0; k < automatasArray[i].alphabet.size(); k++) {
+            for (int k = 0; k < automatasArray[i].alphabet.size(); k++)
+            {
                 cout << automatasArray[i].functions[j][k] << " ";
             }
             cout << endl;
@@ -121,7 +138,8 @@ void showDfasArray(const vector<Automata>& automatasArray){
     }
 }
 
-void separateStates(Automata dfa) {
+vector<int> separateStates(Automata dfa)
+{
     int n = dfa.number_states;
 
     vector<int> totalStates;
@@ -129,17 +147,21 @@ void separateStates(Automata dfa) {
     vector<int> nonFinalStates;
 
     // Fill the vector totalStates with all states of the DFA
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         totalStates.push_back(i);
     }
 
     // Classify the states in Finals and Non-Finals
-    for (int state : totalStates) {
-        if (find(finalStates.begin(), finalStates.end(), state) == finalStates.end()) {
+    for (int state : totalStates)
+    {
+        if (find(finalStates.begin(), finalStates.end(), state) == finalStates.end())
+        {
             nonFinalStates.push_back(state);
         }
     }
 
+    /*
     // Show Non-Final states
     cout << "Non-Final states: ";
     for (int state : nonFinalStates) {
@@ -153,5 +175,64 @@ void separateStates(Automata dfa) {
         cout << state << " ";
     }
     cout << endl;
+*/
+    return nonFinalStates;
+}
 
+void checkStateEquivalence(const Automata& dfa) {
+    vector<int> stateClass(dfa.number_states, 0);
+    set<int> finalSet(dfa.finals.begin(), dfa.finals.end());
+
+    for (int i = 0; i < dfa.number_states; i++) {
+        stateClass[i] = (finalSet.count(i) > 0) ? 1 : 2;
+    }
+
+    bool changed;
+    do {
+        changed = false;
+        map<vector<int>, int> newClasses;
+        vector<int> newStateClass(dfa.number_states);
+        int classId = 1;
+
+        for (int i = 0; i < dfa.number_states; i++) {
+            vector<int> key = {stateClass[i]};
+            for (int j : dfa.functions[i]) {
+                key.push_back(stateClass[j]);
+            }
+
+            if (newClasses.find(key) == newClasses.end()) {
+                newClasses[key] = classId++;
+            }
+            newStateClass[i] = newClasses[key];
+        }
+
+        if (newStateClass != stateClass) {
+            stateClass = newStateClass;
+            changed = true;
+        }
+    } while (changed);
+
+    // Generar pares de equivalencias en lugar de conjuntos
+    set<pair<int, int>> equivalencePairs;
+    map<int, vector<int>> partitions;
+    for (int i = 0; i < dfa.number_states; i++) {
+        partitions[stateClass[i]].push_back(i);
+    }
+
+    cout << "Equivalence pairs for this DFA:" << endl;
+    for (const auto& part : partitions) {
+        const vector<int>& states = part.second;
+        if (states.size() > 1) {
+            for (size_t i = 0; i < states.size(); i++) {
+                for (size_t j = i + 1; j < states.size(); j++) {
+                    equivalencePairs.insert({states[i], states[j]});
+                }
+            }
+        }
+    }
+
+    for (const auto& p : equivalencePairs) {
+        cout << "(" << p.first << ", " << p.second << ") ";
+    }
+    cout << endl;
 }
