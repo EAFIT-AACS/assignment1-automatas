2ef7ae1a0aac66ad1a1c287d2ea50077a400a23b
Salazar <sebastiansalazar4toc@gmail.com>
2025-02-22 22:46:13 -0500

    Se agrega el proyecto, el archivo principal y archivo .txt con la información de los Automatas Finitos Deterministas (DFA's)

---
+++ main.cpp
@@ -0,0 +1,34 @@
+// Libraries
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <vector>
+
+using namespace std;
+
+// Structure
+struct Automata {
+
+    int states;
+    vector<char> alphabet;
+    vector<int> finals;
+    vector<vector <int>> functions;
+
+};
+
+// Number of cases
+int numberOfCases = 0;
+
+// Automata´s Array
+Automata array[numberOfCases];
+
+int main()
+{
+
+    return 0;
+}
+
+void cargarAutomatas(Automata array[numberOfCases]) {
+
+}
